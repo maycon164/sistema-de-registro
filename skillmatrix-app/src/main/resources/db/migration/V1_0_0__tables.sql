4dd3b46ff2693c6938b7e62a7e1729ef3fe02d25
maycon <mayconfelipe164@gmail.com>
2023-06-20 21:20:39 -0300

    add tables

--- skillmatrix-app/src/main/resources/db/migration/V1_0_0__tables.sql
+++ skillmatrix-app/src/main/resources/db/migration/V1_0_0__tables.sql
@@ -1,12 +1,78 @@
+CREATE TABLE profile (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE label (
+    id SERIAL PRIMARY KEY,
+    label TEXT
+);
+
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     name TEXT,
     email TEXT,
     is_active BOOLEAN DEFAULT TRUE,
+    created_at DATE DEFAULT CURRENT_DATE,
+    label_id INTEGER,
+    profile_id INTEGER,
+
+    FOREIGN KEY (label_id) REFERENCES label(id),
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
+
+CREATE TABLE skills (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    label_id INTEGER,
+
+    FOREIGN KEY (label_id) REFERENCES label(id)
+);
+
+CREATE TABLE snapshots(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    created_at DATE DEFAULT CURRENT_DATE,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE snapshot_answer(
+    id SERIAL PRIMARY KEY,
+    snapshot_id INTEGER,
+    skill_id INTEGER,
+    rating INTEGER,
+
+    FOREIGN KEY(snapshot_id) REFERENCES snapshots(id),
+    FOREIGN KEY(skill_id) REFERENCES skills(id)
+);
+
+
+CREATE TABLE clients(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    description TEXT,
     created_at DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE profiles (
+CREATE TABLE teams(
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name TEXT,
+    description TEXT,
+    created_at DATE DEFAULT CURRENT_DATE,
+    team_leader_id INTEGER,
+    client_id INTEGER,
+
+    FOREIGN KEY(client_id) REFERENCES clients(id),
+    FOREIGN KEY(team_leader_id) REFERENCES users(id)
 );
+
+CREATE TABLE team_member(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    team_id INTEGER,
+
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(team_id) REFERENCES teams(id)
+)
